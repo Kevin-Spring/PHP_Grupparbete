2c817615a1d913d8ef667b90bb1ac897772b0f19
Kevin <kevinspring93@hotmail.com>
2020-02-17 10:13:34 +0100

    add READEME-file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $title = "Grupparbete | Start";
 include("header.php");
 ?>
 
-<p><img src="Orson_2.jpg" alt=""></p>
+<p><img src="Orson_2.jpg" alt="" height="700px" width="550px"></p>
 
 <?php 
 include("footer.php");
